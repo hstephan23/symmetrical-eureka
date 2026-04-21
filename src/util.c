55230db9ac00cc42a5c0226f1e281668028ea78a
hstephan23 <harrison@stephanclan.com>
2026-04-20 18:38:13 -0600

    step01

--- src/util.c
+++ src/util.c
@@ -1,5 +1,17 @@
-//
-// Created by Harrison Stephan on 4/20/26.
-//
+/*
+ * util.c -- implementations of the helpers declared in util.h.
+ */
 
 #include "util.h"
+
+#include <string.h>
+
+void remove_newline_from_input(char *str)
+{
+    /* Guard against two edge cases:
+     *   - empty string: strlen == 0, so str[n-1] would be str[-1] (UB).
+     *   - no trailing newline (e.g., EOF without '\n'): don't chop a real char.
+     */
+    const size_t n = strlen(str);
+    if (n > 0 && str[n - 1] == '\n') str[n - 1] = '\0';
+}
