a3a9d99fd18431354716ae802299c22db48070c3
hstephan23 <harrison@stephanclan.com>
2026-04-30 15:44:11 -0600

    feat: add editor command prompt state

--- include/tide/editor.h
+++ include/tide/editor.h
@@ -6,6 +6,13 @@
 #include "tide/buffer.h"
 #include "tide/status.h"
 
+#define TIDE_EDITOR_COMMAND_CAPACITY 128
+
+typedef enum TideEditorPromptMode {
+    TIDE_EDITOR_PROMPT_CLOSED = 0,
+    TIDE_EDITOR_PROMPT_COMMAND
+} TideEditorPromptMode;
+
 typedef enum TideEditorMove {
     TIDE_EDITOR_MOVE_UP,
     TIDE_EDITOR_MOVE_DOWN,
@@ -19,6 +26,9 @@ typedef struct TideEditor {
     size_t viewport_line;
     size_t viewport_column;
     char status[128];
+    TideEditorPromptMode prompt_mode;
+    char command[TIDE_EDITOR_COMMAND_CAPACITY];
+    size_t command_length;
 } TideEditor;
 
 void tide_editor_init(TideEditor *editor, TideBuffer *buffer);
@@ -28,5 +38,11 @@ TideStatus tide_editor_backspace(TideEditor *editor);
 void tide_editor_move(TideEditor *editor, TideEditorMove move);
 void tide_editor_set_status(TideEditor *editor, const char *message);
 void tide_editor_ensure_cursor_visible(TideEditor *editor, size_t width, size_t height);
+void tide_editor_open_command_prompt(TideEditor *editor);
+void tide_editor_cancel_command_prompt(TideEditor *editor);
+int tide_editor_command_active(const TideEditor *editor);
+TideStatus tide_editor_command_insert_char(TideEditor *editor, char ch);
+void tide_editor_command_backspace(TideEditor *editor);
+const char *tide_editor_command_text(const TideEditor *editor);
 
 #endif
